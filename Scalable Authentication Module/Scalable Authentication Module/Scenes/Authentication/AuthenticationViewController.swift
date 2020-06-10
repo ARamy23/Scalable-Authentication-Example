98a70e68a9b47b01767c3794e7b78ea4e2a07e21
Ahmed Ramy <dev.ahmedramy@icloud.com>
2020-06-10 22:57:18 +0200

    Add Xcode project

---
+++ Scalable Authentication Module/Scalable Authentication Module/Scenes/Authentication/AuthenticationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthenticationViewController.swift
+//  Scalable Authentication Module
+//
+//  Created by Ahmed Ramy on 6/10/20.
+//  Copyright © 2020 Ahmed Ramy. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBAction func loginWithEmail(_ sender: Any) {
+        
+    }
+    
+    @IBAction func registerWithEmail(_ sender: Any) {
+        
+    }
+    
+    @IBAction func loginWithGoogle(_ sender: Any) {
+        
+    }
+    
+    @IBAction func loginWithApple(_ sender: Any) {
+        
+    }
+}
