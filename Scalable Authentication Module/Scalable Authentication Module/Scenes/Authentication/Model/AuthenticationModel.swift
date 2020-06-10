3d5f4d75ccf2ec943a10de336e0036034b8db880
Ahmed Ramy <dev.ahmedramy@icloud.com>
2020-06-10 23:08:11 +0200

    Create Authentication Model

---
+++ Scalable Authentication Module/Scalable Authentication Module/Scenes/Authentication/Model/AuthenticationModel.swift
@@ -0,0 +1,27 @@
+//
+//  AuthenticationModel.swift
+//  Scalable Authentication Module
+//
+//  Created by Ahmed Ramy on 6/10/20.
+//  Copyright © 2020 Ahmed Ramy. All rights reserved.
+//
+
+import Foundation
+
+class AuthenticationModel {
+    func loginWithEmail(_ email: String, _ password: String) {
+        
+    }
+    
+    func registerWithEmail(_ email: String, _ password: String) {
+        
+    }
+    
+    func loginWithGoogle() {
+        
+    }
+    
+    func loginWithApple() {
+        
+    }
+}
